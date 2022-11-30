5a763853aecaafacb8be970d0cfa61fd1649d684
sangki810 <113147507+sangki810@users.noreply.github.com>
2022-11-30 08:36:37 -0500

    completed Thought.js file and helpers.js file

---
+++ models/Thought.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const { format_date } = require('../utils/helpers');
+
+const thoughtSchema = new Schema(
+    {
+        toughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => {
+                return format_date(date)
+            },
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    },
+);
+
+// gets total count of reactions using virtual reactionCount
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+// creates Thought model using thoughtSchema
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
